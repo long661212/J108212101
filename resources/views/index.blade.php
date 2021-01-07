0d9b53c5a80075f8416136772070eb4b5b3427b0
long661212 <long661212@gmail.com>
2021-01-07 13:30:06 +0800

    first commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,14 +8,14 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
-    <title>何敏煌</title>
+    <title>黃國榮</title>
   </head>
   <body>
   	<div class="container">
-	    <h1>何敏煌的期末作業</h1>
+	    <h1>黃國榮的期末作業</h1>
 	    <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">J107001119</a>
+    <a class="navbar-brand" href="#">J108212101</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
